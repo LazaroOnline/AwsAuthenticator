fa4d823d5d0a8e01062fee5a5979bd28a594984e
Lazaro <lazaroonline@gmail.com>
2025-12-18 02:31:18 +0100

    Update build script.

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -1,27 +1,53 @@
+# BUILD APP
 
-cd ..
+# Navitage to the solution folder:
+$scriptFolder = if ($PSScriptRoot -eq "") { "." } else { $PSScriptRoot } # Support PSv5/Core running from script file or copy/pasted into a prompt.
+cd "$scriptFolder/.."
+Write-Host -F Blue "Changed dir to: '$PWD'"
+
+
+# CONFIG:
+$csprojPath = "src/AwsAuthenticator.UI/AwsAuthenticator.UI.csproj"
+$appFileName = "AwsAuthenticator" # .exe
 $publishFolder = "Publish"
 $publishFolderApp = "$publishFolder/app"
 
-# Cleanup:
+
+# CLEANUP:
 Write-Host -F Blue "Cleaning up folders: '$publishFolderApp' and '$publishFolderDotnetTool'..."
-if (test-path $publishFolderApp       ) { Remove-Item "$publishFolderApp/*"        -Force -Recurse; Write-Host -F Yellow "Removed folder: $publishFolderApp" }
+if (test-path $publishFolderApp) { Remove-Item "$publishFolderApp/*" -Force -Recurse; Write-Host -F Yellow "Removed folder: $publishFolderApp" }
 Write-Host -F Blue "Starting compilation..."
+
 
 # COMPILE AS EXE:
 # https://learn.microsoft.com/en-us/dotnet/core/tools/dotnet-publish
 Write-Host -F Blue "Publishing app..."
-dotnet publish "src/AwsAuthenticator.UI/AwsAuthenticator.UI.csproj" -c "Release" -o $publishFolderApp /p:DebugType=None -p:PublishSingleFile=true --self-contained false # -r "win-x64"
+dotnet publish $csprojPath `
+--self-contained false `
+-o $publishFolderApp `
+-c "Release" `
+-p:DebugType=None `
+-p:EnableCompressionInSingleFile=false `
+-p:PublishAot=false `
+-p:PublishTrimmed=false `
+-p:PublishSingleFile=true 
+# Enabling "PublishTrimmed" trimming works but breaks the "Jot" library used in "WindowStateTracker.cs" to remember the window position during restarts, and throws warnings for "ReactiveUI.WhenAnyMixin.WhenAnyValue<>()".
+# -r "win-x64" 
+$compiledAppFile = Get-Item "$publishFolderApp/$appFileName.exe" -ErrorAction SilentlyContinue
+if ($compiledAppFile -eq $null) {
+	Write-Host -F Red "Error compiling/publishing ('$appFileName.exe' not created)."
+	return
+}
 Write-Host -F Green "Publishing app DONE!"
+return
 
-$version = (Get-Item "$publishFolderApp/AwsAuthenticator.exe").VersionInfo.FileVersion
-
-
-$destinationZip = "$publishFolder/AwsAuthenticator-v$version.zip"
+# COMPRESS TO ZIP:
+$version = $compiledAppFile.VersionInfo.FileVersion
+$destinationZip = "$publishFolder/$appFileName-v$version.zip"
 Write-Host -F Blue "Compressing binaries into: '$destinationZip'..."
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.archive/compress-archive?view=powershell-7.3
 Compress-Archive -Path "$publishFolderApp/*" -DestinationPath $destinationZip -Force
 
-Start $publishFolder
 
+Start $publishFolder
 Write-Host -F Green "Finished Build!"
